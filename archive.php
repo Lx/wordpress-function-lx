bfa4b3ba7f10f7e6a105b0b9d6b26e905c2035be
Alex Peters <alex@peters.net>
2011-01-03 20:41:38 +1100

    display dates as per configured WordPress options

--- archive.php
+++ archive.php
@@ -37,7 +37,7 @@ get_header(); ?>
 <?php endif; ?>
 <div class="posttop">
 <h2 class="posttitle"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-<div class="postmetatop">Posted <?php the_time('M.d, Y') ?> under <?php the_category(', ') ?></div><!-- /postmetatop -->
+<div class="postmetatop">Posted <?php the_date() ?> under <?php the_category(', ') ?></div><!-- /postmetatop -->
 </div><!-- /posttop -->
         
 <div class="postcontent">
