75fd78f11f4815aefe1d5751608fc43fa9c7129b
Alex Peters <alex@peters.net>
2011-01-20 15:23:36 +1100

    abstract post generation from index page
    Place the logic for outputting posts into its own file.  The intention
    is to make other similar pages (like archive.php and search.php) call
    this file soon instead of declaring the same logic redundantly.

--- index.php
+++ index.php
@@ -10,33 +10,7 @@ get_header(); ?>
 
 
 <?php if (have_posts()) : ?>
-<?php while (have_posts()) : the_post(); ?>
-     
-<?php if (function_exists('wp_list_comments')): ?>
-<div <?php post_class(post); ?> id="post-<?php the_ID(); ?>">
-<?php else : ?>
-<div class="post" id="post-<?php the_ID(); ?>">
-<?php endif; ?>
-<div class="posttop">
-<h2 class="posttitle"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-<div class="postmetatop">Posted <?php the_date() ?> under <?php the_category(', ') ?></div><!-- /postmetatop -->
-</div><!-- /posttop -->
-        
-<div class="postcontent">
-<?php the_content('(Read More &raquo;)'); ?>
-<div class="cleared"></div>
-</div><!-- /postcontent -->
-        
-<?php if (get_the_tags() || comments_open()) : ?>
-<div class="postmetabottom">
-<div class="tags"><?php the_tags('Tags: ', ', ', ''); ?></div>
-<div class="metacomments"><?php if ( comments_open() ) : ?><span><?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?></span><?php endif; ?></div>
-<div class="cleared"></div>
-</div><!-- /postmetabottom -->
-<?php endif; ?>
-</div><!-- /post -->
-
-<?php endwhile; ?>
+  <?php get_template_part('loop', 'index'); ?>
 <?php else : ?>
 <div class="post">
 <div class="posttop">
